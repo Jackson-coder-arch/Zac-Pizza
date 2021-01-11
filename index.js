85800c0d48fbb2bdb5e66fa31b99d503954d1d19
Jackson-coder-arch <jacksonikonya@gmail.com>
2021-01-11 09:23:10 +0300

    value reset and collection

--- index.js
+++ index.js
@@ -11,47 +11,47 @@ function getPizzaOrder(pizzaType,crustType,extraToppings,pizzaQuantity,delivery,
     this.deliveryPrice = 0;
     this.price = 0;
 };
-// getPizzaOrder.prototype.finalPrice = function () {
-//     if (this.pizzaSize === "small") {
-//         this.sizePrice = 400;
-//     } else if (this.pizzaSize === "medium") {
-//         this.sizePrice = 700;
-//     }else if (this.pizzaSize === "large") {
-//         this.sizePrice = 1500;
-//     };
-//     if (this.crustType === "stuffed") {
-//         this.crustPrice = 150;
-//     } else if (this.crustType === "thick") {
-//         this.crustPrice = 200;
-//     }else if (this.crustType === "Crispy") {
-//         this.crustPrice = 150;
-//     }else if (this.crustType === "gluten-free") {
-//         this.crustPrice = 200;
-//     };
-//     if (this.pizzaSize === "small") {
-//         this.toppingPrice = 100;
-//     }else if (this.pizzaSize === "medium") {
-//         this.toppingPrice = 150;
-//     }else if (this.pizzaSize === "large") {
-//         this.toppingPrice = 200;
-//     };
-//     this.price =(this.sizePrice +this.crustPrice + this.toppingPrice) * this.pizzaQuantity;
-// };
-// getPizzaOrder.prototype.toBeDelivered = function () {
-//     if (this.delivery === "deliver") {
-//         this.price += 300;
-//     }else if(this.delivery ==="collect") {
-//         this.price +=0;
-//     };
-// };
-// function resetFieldValues () {
-//     pizzaSize = "";
-//     pizzaType = "";
-//     extraToppings = "";
-//     crustType = "";
-//     $("#pizza-quantity").val("");
-// };
-// //  start of my user logic
+getPizzaOrder.prototype.finalPrice = function () {
+    if (this.pizzaSize === "small") {
+        this.sizePrice = 400;
+    } else if (this.pizzaSize === "medium") {
+        this.sizePrice = 700;
+    }else if (this.pizzaSize === "large") {
+        this.sizePrice = 1500;
+    };
+    if (this.crustType === "stuffed") {
+        this.crustPrice = 150;
+    } else if (this.crustType === "thick") {
+        this.crustPrice = 200;
+    }else if (this.crustType === "Crispy") {
+        this.crustPrice = 150;
+    }else if (this.crustType === "gluten-free") {
+        this.crustPrice = 200;
+    };
+    if (this.pizzaSize === "small") {
+        this.toppingPrice = 100;
+    }else if (this.pizzaSize === "medium") {
+        this.toppingPrice = 150;
+    }else if (this.pizzaSize === "large") {
+        this.toppingPrice = 200;
+    };
+    this.price =(this.sizePrice +this.crustPrice + this.toppingPrice) * this.pizzaQuantity;
+};
+getPizzaOrder.prototype.toBeDelivered = function () {
+    if (this.delivery === "deliver") {
+        this.price += 300;
+    }else if(this.delivery ==="collect") {
+        this.price +=0;
+    };
+};
+function resetFieldValues () {
+    pizzaSize = "";
+    pizzaType = "";
+    extraToppings = "";
+    crustType = "";
+    $("#pizza-quantity").val("");
+};
+//  start of my user logic
 //  $(document).ready(function() {
 //      var modal = $(".ordering-plate");
 //      var placeOrder = $(".place-order");
